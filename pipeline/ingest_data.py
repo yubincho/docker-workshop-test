23b00d7d381ce424e957a292eb4889c4c993e561
yubincho <yubincho9@gmail.com>
2026-01-19 01:19:41 +0000

    change code

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click 
 
 
 dtype = {
@@ -32,20 +33,17 @@ parse_dates = [
 ]
 
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host= 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 10000
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_pass', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=10000, type=int, help='Chunk size for ingestion')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize, year, month):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
